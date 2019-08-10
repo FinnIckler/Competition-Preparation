c40a4f78c1bb457fe83e5073743b375acf7b221b
FinnIckler <finnickler@gmail.com>
2019-08-10 21:45:52 +0300

    Moved /persons api calls the api.wca method

--- src/lib/api/wca/persons.py
+++ src/lib/api/wca/persons.py
@@ -15,3 +15,25 @@ def get_wca_competitor(wca_id):
     competitor_info = response.json(wca_id)
     return competitor_info
 
+
+def get_wca_competitors(wca_ids):
+    competitors_info = []
+    BATCH_SIZE = 100
+    # Split WCA IDs up into batches of 100 each time and request information from WCA API
+    for competitors in range(0, math.ceil(len(wca_ids) / BATCH_SIZE)):
+        wca_ids_partial = wca_ids[
+            competitors * BATCH_SIZE:(competitors + 1) * BATCH_SIZE
+        ]
+        url = "https://www.worldcubeassociation.org/api/v0/persons?wca_ids={}&per_page={}".format(
+            ",".join(wca_ids_partial), BATCH_SIZE
+        )
+
+        response = requests.get(url)
+
+        if not response.ok:
+            Logger.error("No connection to the WCA or Malformed URL")
+            raise API_ERROR("get_wca_competitors failed")
+
+        competitors_info.extend(response.json())
+
+    return competitors_info
